9657844767c7d01faf9a08bc5ee2cd9c346d1baa
MMJ <my_name@example.com>
2023-11-05 13:49:45 +0900

    完全に改修した

--- src/components/noble/NobleContainer.vue
+++ src/components/noble/NobleContainer.vue
@@ -15,9 +15,15 @@ onMounted(async () => {
   asyncData = await asyncGetData();
 });
 
+// propsとemitsの定義
 const props = defineProps(["items"]);
-const emits = defineEmits(["skillValue"]);
+const emits = defineEmits(["nobleValue"]);
 
+// propsの変更を監視
+const servantIndex = ref(null);
+const nobleData = ref([{ Detail: "ここに宝具の詳細" }]);
+const nobleName = ref("サーヴァント未選択");
+const isActiveList = ref(Array(20).fill(true));
 watch(props, (newValue) => {
   servantIndex.value = "s" + newValue.items;
   const objectList = asyncData.filter(
@@ -28,52 +34,74 @@ watch(props, (newValue) => {
   const maxId = Math.max(...objectList.map((obj) => obj.EntityID));
   nobleData.value = objectList.filter((obj) => obj.EntityID == maxId);
   nobleName.value = nobleData.value[0].SkillName;
+  // isActiveListの初期化
+  for (let i = 0; i < isActiveList.value.length; i++) {
+    isActiveList.value[i] = true;
+  }
   bufftype();
 });
 
-const servantIndex = ref(null);
-const nobleData = ref([{ Detail: "ここに宝具の詳細" }]);
-const nobleName = ref("サーヴァント未選択");
+// 宝具レベルの変更を監視
 const nobleLevel = ref("Value0");
-const nobleOC = ref("Value0");
-const init = {
-  宝具攻撃: 0,
-  攻撃力: 0,
-  Busterカード性能: 0,
-  Quickカード性能: 0,
-  Artsカード性能: 0,
-  宝具威力: 0,
-};
-const nobleValue = ref(null);
-
 function handleNobleLevel(item) {
   nobleLevel.value = `Value` + (item - 1);
   bufftype();
 }
+
+// 宝具OCの変更を監視
+const nobleOC = ref("Value0");
 function handleNobleOC(item) {
   nobleOC.value = `Value` + (item - 1);
   bufftype();
 }
+
+// 文字列を数値に変換したりしなかったり関数
+function parseFloatValue(item) {
+  const value = parseFloat(item);
+  if (isNaN(value)) {
+    return item;
+  } else {
+    return value;
+  }
+}
+
+// 宝具の効果を計算する関数
 function bufftype() {
-  const result = []; // 結果の配列を初期化
+  const nobleObjList = []; // 宝具情報のの配列を初期化
+  const nobleValue = {}; // 最終的な効果の配列を初期化
+  //生データからOCやLvを反映した実バフ値を取り出し、オブジェクトに変換
   nobleData.value.forEach((item) => {
     const obj = {}; // 各ケースごとに新しいオブジェクトを作成
     switch (item.Grow) {
       case "Lv":
-        obj[item.MainText] = item[nobleLevel.value];
-        result.push(obj);
+        obj[item.MainText] = parseFloatValue(item[nobleLevel.value]);
+        nobleObjList.push(obj);
         break;
       case "OC":
-        obj[item.MainText] = item[nobleOC.value];
-        result.push(obj);
+        obj[item.MainText] = parseFloatValue(item[nobleOC.value]);
+        nobleObjList.push(obj);
         break;
       default:
-        obj[item.MainText] = item["Value3"];
-        result.push(obj);
+        obj[item.MainText] = parseFloatValue(item["Value3"]);
+        nobleObjList.push(obj);
         break;
     }
   });
-  nobleValue.value = result; // 最終的な結果を代入
+  for (var i = 0; i < nobleObjList.length; i++) {
+    if (isActiveList.value[i] == false) {
+      continue;
+    }
+    for (var key in nobleObjList[i]) {
+      if (nobleValue[key] == undefined) {
+        nobleValue[key] = nobleObjList[i][key];
+      } else {
+        nobleValue[key] += nobleObjList[i][key];
+      }
+    }
+  }
+  //console.log("Noble処理前", nobleObjList);
+  //console.log("Noble処理後", nobleValue);
+  emits("nobleValue", nobleValue);
 }
 </script>
 
@@ -89,12 +117,19 @@ function bufftype() {
                 ><span v-html="nobleName"></span
               ></v-list-item>
             </template>
-            <v-list-item v-for="item in nobleData"
-              >{{ item.Detail }}
-              <span v-if="item.Grow == 'Lv'">{{ item[nobleLevel] }}</span>
-              <span v-if="item.Grow == 'OC'">{{
-                item[nobleOC]
-              }}</span></v-list-item
+            <v-list-item v-for="(item, index) in nobleData" :key="item"
+              ><v-layout>
+                <v-checkbox-btn
+                  v-model="isActiveList[index]"
+                  @change="bufftype"
+                  hide-details
+                ></v-checkbox-btn>
+                {{ item.Detail }}
+                <span v-if="item.Grow == 'Lv'">{{ item[nobleLevel] }}</span>
+                <span v-if="item.Grow == 'OC'">{{
+                  item[nobleOC]
+                }}</span></v-layout
+              ></v-list-item
             >
           </v-list-group>
         </v-list>
@@ -109,4 +144,3 @@ function bufftype() {
     <v-row> </v-row>
   </v-container>
 </template>
-
